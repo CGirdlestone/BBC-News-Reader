da597df9eabd515d16e355d54d7e488340dfa7ed
CGirdlestone <45768379+CGirdlestone@users.noreply.github.com>
2018-12-13 19:17:48 +0000

    Add files via upload
    Changed to explicit imports instead of wildcard imports.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ main.py
 This will run the BBC News Reader app and implement the GUI.
 """
 
-from BBC_gui import *
-from BBC_news import *
+from BBC_gui import Application
+from BBC_news import BBCNewsReader 
 
 # Create an instance of the application.
 app = Application(BBCNewsReader())
